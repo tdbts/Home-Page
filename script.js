4da76b4e05d95183c1a7867d050f9f830e6f8bca
tdbts <VinnySanchez87@gmail.com>
2014-10-28 15:15:25 -0400

    Added some autoscrolling functionality, and got the twitter/github links set up...about to start on creating a modal for when the user clicks on the email icon

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
+	
+	function scrollUpToTop(milliseconds) {
+
+		$('html, body').animate({
+
+			scrollTop: 0
+		}, milliseconds);
+	}	
+
+	function scrollDownTo(elementSelector, milliseconds) {
+
+		$('html, body').animate({
+
+			scrollTop: $(elementSelector).offset().top
+		}, milliseconds);
+	}
+	
+	function attachLink(selector, url) {
+		
+		$(selector).on('click', function() {
+			
+			window.open(url);
+		});
+	}
 
 	$('.bar-icon').tooltip();
+
+	scrollUpToTop(500);
+
+	$('#welcomeButton').on('click', function() {
+		
+		scrollDownTo('#more_info', 750);
+	});
+
+	attachLink('#twitter_icon', 'http://www.twitter.com/VRSanchez8717');
+	attachLink('#github_icon', 'http://www.github.com/tdbts');
 
 });
